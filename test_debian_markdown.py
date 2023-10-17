5cf29204c7c7e519b262783a661f5aaa976b8538
Boaz Leleina <63369143+boazleleina@users.noreply.github.com>
2023-10-17 19:22:29 +0300

    Update test_debian_markdown.py

--- test_debian_markdown.py
+++ test_debian_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from bs4 import BeautifulSoup
-from debian_markdown import convert_tag_to_markdown
+from debian_markdown import get_parse_debian, convert_tag_to_markdown, markdown_file
+import requests
+from unittest.mock import patch, Mock, mock_open, MagicMock
 
 class TestConvertTagToMarkdown(unittest.TestCase):
     def test_paragraph_conversion(self):
@@ -21,8 +23,6 @@ class TestConvertTagToMarkdown(unittest.TestCase):
         result = convert_tag_to_markdown(BeautifulSoup(html, 'html.parser').ul)
         self.assertEqual(result, expected)
 
-
-
     def test_list_item_conversion(self):
         html = '<li>List item</li>'
         expected = '- List item\n'
@@ -41,6 +41,37 @@ class TestConvertTagToMarkdown(unittest.TestCase):
         expected = ''
         result = convert_tag_to_markdown(BeautifulSoup(html, 'html.parser').div)
         self.assertEqual(result, expected)
+
+class TestDebianMarkdownFunctions(unittest.TestCase):
+    @patch('debian_markdown.requests.get')
+    def test_get_parse_debian(self, mock_get):
+        # Mock the response from the server
+        mock_response = Mock()
+        mock_response.status_code = 200
+        mock_response.text = '<p>Test paragraph</p>'
+        mock_get.return_value = mock_response
+
+        # Test the function
+        result = get_parse_debian()
+
+        # Ensure that BeautifulSoup is called with the correct HTML
+        mock_response.raise_for_status.assert_called()
+        self.assertIsInstance(result, BeautifulSoup)
+        
+    def test_markdown_file(self):
+        # Mock the BeautifulSoup object with sample content
+        html = '<p>Test paragraph</p><h2>Test heading</h2>'
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Call the function and capture the written content
+        markdown_file(soup)
+            
+        # Check if the content was written to the file correctly
+        with open('debian_wiki.md', 'r', encoding='utf-8') as file:
+            written_content = file.read()
+            expected_content = "Test paragraph\n\n## Test heading\n\n"
+            self.assertEqual(written_content, expected_content)
+
 
 if __name__ == '__main__':
     unittest.main()
