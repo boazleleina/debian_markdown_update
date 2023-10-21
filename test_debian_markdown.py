634127ccd0e4ce65c4a2881fe9fc1de0ee580622
Boaz Leleina <63369143+boazleleina@users.noreply.github.com>
2023-10-21 21:01:51 +0300

    Update test_debian_markdown.py

--- test_debian_markdown.py
+++ test_debian_markdown.py
@@ -1,8 +1,7 @@
 import unittest
 from bs4 import BeautifulSoup
 from debian_markdown import get_parse_debian, convert_tag_to_markdown, markdown_file
-import requests
-from unittest.mock import patch, Mock, mock_open, MagicMock
+from unittest.mock import patch, Mock
 
 class TestConvertTagToMarkdown(unittest.TestCase):
     def test_paragraph_conversion(self):
@@ -45,19 +44,29 @@ class TestConvertTagToMarkdown(unittest.TestCase):
 class TestDebianMarkdownFunctions(unittest.TestCase):
     @patch('debian_markdown.requests.get')
     def test_get_parse_debian(self, mock_get):
-        # Mock the response from the server
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_response.text = '<p>Test paragraph</p>'
-        mock_get.return_value = mock_response
+        # Case 1: Simulate a successful response (status code 200)
+        mock_response_200 = Mock()
+        mock_response_200.status_code = 200
+        mock_response_200.text = '<p>Test paragraph</p'
+        mock_get.return_value = mock_response_200
 
-        # Test the function
-        result = get_parse_debian()
-
+        # Test the function for a successful response (status code 200)
+        result_200 = get_parse_debian()
         # Ensure that BeautifulSoup is called with the correct HTML
-        mock_response.raise_for_status.assert_called()
-        self.assertIsInstance(result, BeautifulSoup)
+        mock_response_200.raise_for_status.assert_called()
+        self.assertIsInstance(result_200, BeautifulSoup)
+
+        # Case 2: Simulate a non-successful response (status code other than 200)
+        mock_response_not_200 = Mock()
+        mock_response_not_200.status_code = 404  # Simulate a non-200 status code (you can change it to any other status code)
+        mock_get.return_value = mock_response_not_200
+        # Test the function for a non-successful response (status code other than 200)
+        with self.assertRaises(Exception) as context:
+            get_parse_debian()
         
+        self.assertEqual(str(context.exception), "Failed to fetch the Debian Wiki News page: 404")
+            # Test the function for a non-successful response (status code other than 200)
+
     def test_markdown_file(self):
         # Mock the BeautifulSoup object with sample content
         html = '<p>Test paragraph</p><h2>Test heading</h2>'
@@ -65,13 +74,12 @@ class TestDebianMarkdownFunctions(unittest.TestCase):
 
         # Call the function and capture the written content
         markdown_file(soup)
-            
+
         # Check if the content was written to the file correctly
         with open('debian_wiki.md', 'r', encoding='utf-8') as file:
             written_content = file.read()
             expected_content = "Test paragraph\n\n## Test heading\n\n"
             self.assertEqual(written_content, expected_content)
-
 
 if __name__ == '__main__':
     unittest.main()
